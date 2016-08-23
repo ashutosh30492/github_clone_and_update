35b9826c089c7b3210d4e1f4cc91ffe772c5844b
ASHUTOSH SHARMA <ashutosh30492@gmail.com>
2016-08-23 14:41:14 +0530

    Add script to clone all private remote repositories

---
+++ clone_all.sh
@@ -0,0 +1,1 @@
+curl -u {{USERNAME}} -s https://api.github.com/orgs/{{ORGANIZATION}}/repos?per_page=200 | ruby -rubygems -e 'require "json"; JSON.load(STDIN.read).each { |repo| %x[git clone #{repo["ssh_url"]} ]}'
